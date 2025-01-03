e2f5d0790ba41aeddd1c44a74f1c2653b97bd2e8
arnavdeepaware <arnav.deepaware@gmail.com>
2025-01-03 17:11:58 -0500

    Added Birria-Landia card

--- unit-1/community-board/src/App.jsx
+++ unit-1/community-board/src/App.jsx
@@ -8,9 +8,10 @@ const App = () => {
       <Header />
       <div className='main-container'>
         <Card 
-          title='Halal Guys' 
-          description='Middle Eastern'
-          imageSrc="https://www.vmcdn.ca/f/files/via/images/food/halal-guys-chicken-platter.jpg" 
+          title='Birria-Landia' 
+          description='Mexican'
+          imageSrc="https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"
+          weblink="https://birrialandia.com"
         />
 
         <Card 
--- unit-1/community-board/src/components/Card.jsx
+++ unit-1/community-board/src/components/Card.jsx
@@ -5,8 +5,14 @@ const Card = (props)=> {
         <div className="card">
             <img src={props.imageSrc} alt={props.title} className="card-image" />
             <h3 className="card-title">{props.title}</h3>
-            <p className="card-description">{props.descripiton}</p>
-            <button className="card-button">View Menu</button>
+            <p className="card-description">{props.description}</p>
+            <button className="card-button">
+                <a href={props.weblink}         
+                   target="_blank" 
+                   rel="noopener noreferrer">
+                    View Menu
+                </a>
+            </button>
         </div>
     )
 }
