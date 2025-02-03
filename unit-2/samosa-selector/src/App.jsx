f59bcb9161043a34c794f513725722b3c661dd96
arnavdeepaware <arnav.deepaware@gmail.com>
2025-02-02 21:36:58 -0500

    Step 1: Create the User Interface

--- unit-2/samosa-selector/src/App.jsx
+++ unit-2/samosa-selector/src/App.jsx
@@ -4,7 +4,14 @@ const App = () => {
 
   return (
     <div className="App">
-
+      <div className='header'>
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img 
+          className='samosa' 
+          src="https://wallpapers.com/images/hd/golden-fried-samosa-black-background-2jx66jdwe3w968js-2jx66jdwe3w968js.jpg"  alt='samosa-img'
+          />
+      </div>
     </div>
   )
 }
