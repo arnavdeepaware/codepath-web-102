c65e74ea030025395a44ca1da22daebefdb0137d
arnavdeepaware <arnav.deepaware@gmail.com>
2025-02-02 22:39:56 -0500

    Added background and headers

--- unit-2/geo-flashcards/src/App.jsx
+++ unit-2/geo-flashcards/src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
 
+  const [cards, setCards] = useState(10);
+  const [score, setScore] = useState(0);
+
   return (
     <div className="App">
+      <div className='header'>
+        <h2>The Ultimate Geo-Champion</h2>
+        <h3>How good are you with places? Test all your knowledge here!</h3>
+        <h4>Number of Cards: {cards}</h4>
+
+      </div>
 
     </div>
   )
