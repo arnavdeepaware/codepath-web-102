3d23c9007e26e45c6932d24ac6001c8570fee201
arnavdeepaware <arnav.deepaware@gmail.com>
2025-02-02 21:43:30 -0500

    Step 2: Add a Counter

--- unit-2/samosa-selector/src/App.jsx
+++ unit-2/samosa-selector/src/App.jsx
@@ -1,4 +1,9 @@
 import './App.css';
+import { useState } from 'react';
+
+// const [count, setCount] = useState(0);
+// const [multiplier, setMultiplier] = useState(1);
+// const updateCount = () => setCount(count + multiplier);
 
 const App = () => {
 
@@ -6,10 +11,11 @@ const App = () => {
     <div className="App">
       <div className='header'>
         <h1>Samosa Selector</h1>
-        <h2>Count:</h2>
+        <h2>Count: {count}</h2>
         <img 
           className='samosa' 
           src="https://wallpapers.com/images/hd/golden-fried-samosa-black-background-2jx66jdwe3w968js-2jx66jdwe3w968js.jpg"  alt='samosa-img'
+          onClick={updateCount}
           />
       </div>
     </div>
