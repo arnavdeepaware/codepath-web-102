0a82b505bd349217cb2888d34d94e9ed14c38fad
arnavdeepaware <arnav.deepaware@gmail.com>
2025-02-02 21:46:02 -0500

    Step 2: Fixed Bugs

--- unit-2/samosa-selector/src/App.jsx
+++ unit-2/samosa-selector/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css';
 import { useState } from 'react';
 
-// const [count, setCount] = useState(0);
-// const [multiplier, setMultiplier] = useState(1);
-// const updateCount = () => setCount(count + multiplier);
-
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  const updateCount = () => setCount(count + multiplier);
 
   return (
     <div className="App">
@@ -14,12 +13,13 @@ const App = () => {
         <h2>Count: {count}</h2>
         <img 
           className='samosa' 
-          src="https://wallpapers.com/images/hd/golden-fried-samosa-black-background-2jx66jdwe3w968js-2jx66jdwe3w968js.jpg"  alt='samosa-img'
+          src="https://wallpapers.com/images/hd/golden-fried-samosa-black-background-2jx66jdwe3w968js-2jx66jdwe3w968js.jpg"  
+          alt='samosa-img' 
           onClick={updateCount}
-          />
+        />
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
