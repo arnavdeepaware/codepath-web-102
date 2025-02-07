7098053ef510290a2316d5e295626a86867fc203
arnavdeepaware <arnav.deepaware@gmail.com>
2025-02-07 13:19:46 -0500

    Completed Step 1

---
+++ unit-3/barista-app/src/Components/BaristaForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BaristaForm = () => {
+    const onNewDrink = () => {};
+    const onCheckAnswer = () => {};
+  
+    return (
+        <div>
+            <h2>Hi, I'd like to order a:</h2>
+            <form>
+            </form>
+
+            <button type="submit" className="button submit" onClick={onCheckAnswer}>Check Answer</button>
+            <button type="new-drink-button" className="button submit" onClick={onNewDrink}>New Drink</button>
+        </div>
+    );
+};
+
+export default BaristaForm;
