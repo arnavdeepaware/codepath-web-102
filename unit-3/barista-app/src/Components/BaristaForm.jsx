01868fa515c5d48b4dd9efc1f80d433bc415f7ba
arnavdeepaware <arnav.deepaware@gmail.com>
2025-02-11 17:07:01 -0500

    Added RecipeChoices.jsx

--- unit-3/barista-app/src/Components/BaristaForm.jsx
+++ unit-3/barista-app/src/Components/BaristaForm.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 const BaristaForm = () => {
     const onNewDrink = () => {};
     const onCheckAnswer = () => {};
+
+    const [inputs, setInputs] = useState({
+        temperature: "",
+        milk: "",
+        syrup: "",
+        blended: ""
+    });
+
+    const ingredients = {
+        'temperature' : ['hot', 'lukewarm', 'cold'],
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended': ['yes', 'turbo', 'no']
+      }
   
     return (
         <div>
             <h2>Hi, I'd like to order a:</h2>
             <form>
             </form>
+
+            
 
             <button type="submit" className="button submit" onClick={onCheckAnswer}>Check Answer</button>
             <button type="new-drink-button" className="button submit" onClick={onNewDrink}>New Drink</button>
