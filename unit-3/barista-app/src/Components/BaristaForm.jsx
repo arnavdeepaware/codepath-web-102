9a2dd83f170ebf4ad49d2237a27267a197cbc7aa
arnavdeepaware <arnav.deepaware@gmail.com>
2025-02-11 17:49:18 -0500

    Step 3: Added drinks.json data to forms

--- unit-3/barista-app/src/Components/BaristaForm.jsx
+++ unit-3/barista-app/src/Components/BaristaForm.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { useState } from "react";
 import RecipeChoices from "./RecipeChoices";
+// Fix the import path
+import drinksData from "../drinks.json";
 
 const BaristaForm = () => {
-    const onNewDrink = () => {};
-    const onCheckAnswer = () => {};
+    const getNewDrink = () => {
+        // Use drinksData instead of drinks
+        let randomIndex = Math.floor(Math.random() * drinksData.drinks.length);
+        let currentDrink = drinksData.drinks[randomIndex];
+        setDrink({
+            name: currentDrink.name,
+            ingredients: currentDrink.ingredients
+        });
+        // Set the true recipe from ingredients
+        setTrueRecipe(currentDrink.ingredients);
+    };
+
+    const onNewDrink = () => {
+        setInputs({
+            temperature: "",
+            milk: "",
+            syrup: "",
+            blended: "",
+        });
+
+        getNewDrink();
+    };
+
+    const onCheckAnswer = () => { };
 
     const [inputs, setInputs] = useState({
         temperature: "",
@@ -14,15 +38,22 @@ const BaristaForm = () => {
     });
 
     const ingredients = {
-        'temperature' : ['hot', 'lukewarm', 'cold'],
+        'temperature': ['hot', 'lukewarm', 'cold'],
         'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
         'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
         'blended': ['yes', 'turbo', 'no']
-      }
-  
+    }
+
+    const [drink, setDrink] = useState('');
+    const [trueRecipe, setTrueRecipe] = useState('');
+
     return (
         <div>
             <h2>Hi, I'd like to order a:</h2>
+            <div>
+                <h2 className="mini-header">{drink.name}</h2>
+                <button type="new-drink-button" className="button newdrink" onClick={onNewDrink}>🔄</button>
+            </div>
             <form>
                 <h3>Temperature</h3>
                 <div className="answer-space" >
@@ -81,7 +112,7 @@ const BaristaForm = () => {
                 />
             </form>
 
-            
+
 
             <button type="submit" className="button submit" onClick={onCheckAnswer}>Check Answer</button>
             <button type="new-drink-button" className="button submit" onClick={onNewDrink}>New Drink</button>
